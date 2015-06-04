74747c5d2eca65e8c0bc61847e75b2296e385589
karak2 <karak@sch.bme.hu>
2015-06-04 21:17:47 +0200

    wcf added

--- dummy2/Program.cs
+++ dummy2/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WcfServiceLibrary1;
 
 namespace dummy2
 {
@@ -10,8 +14,22 @@ namespace dummy2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ok");
-            Console.ReadKey();
+            using (ServiceHost host = new ServiceHost(typeof(Service1)))
+            {
+                // Enable metadata publishing.
+               
+
+                // Open the ServiceHost to start listening for messages. Since
+                // no endpoints are explicitly configured, the runtime will create
+                // one endpoint per base address for each service contract implemented
+                // by the service.
+                host.Open();
+
+                Thread.Sleep(-1);
+                
+                // Close the ServiceHost.
+                host.Close();
+            }
         }
     }
 }
